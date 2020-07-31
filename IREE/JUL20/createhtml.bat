8f658b9f88589a230dcabfd22793c4d2eacfd875
Marta Silva <31829224+m0silva@users.noreply.github.com>
2020-07-31 19:01:59 +0100

    Extraction Jul20

---
+++ IREE/JUL20/createhtml.bat
@@ -0,0 +1,6 @@
+pandoc -f markdown -t html -o manual_IREE_JUL2020.html manual_IREE_JUL2020.md
+pandoc -f markdown -t html -o aux_files\codebook\iree_cdbk.html aux_files\codebook\iree_cdbk.md
+pandoc -f markdown -t html -o aux_files\describe_dataset\iree_dscr.html aux_files\describe_dataset\iree_dscr.md
+pandoc -f markdown -t html -o aux_files\descriptive_statistics\iree_stat.html aux_files\descriptive_statistics\iree_stat.md
+pandoc -f markdown -t html -o aux_files\variables_description\iree_vars.html aux_files\variables_description\iree_vars.md
+pandoc -f markdown -t html -o aux_files\variables_description\iree_vars_pt_en.html aux_files\variables_description\iree_vars_pt_en.md
